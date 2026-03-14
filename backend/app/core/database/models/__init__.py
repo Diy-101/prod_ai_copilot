d5328c209c760a3c0f1192973c38b870d744ea73
Finikfin <finikgrib@gmail.com>
2026-03-14 10:54:15 +0300

    add action model

--- backend/app/core/database/models/__init__.py
+++ backend/app/core/database/models/__init__.py
@@ -1,4 +1,5 @@
 from app.core.database.models.base import Base
 from app.core.database.models.user import User, UserRole
+from app.core.database.models.action import Action, HttpMethod
 
-__all__ = ["Base", "User", "UserRole"]
+__all__ = ["Base", "User", "UserRole", "Action", "HttpMethod"]
